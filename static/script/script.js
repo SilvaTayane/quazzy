e76d95014cdecf8f5045c21e00b15e50e3f13e5b
isis-K <91484736+Fabio-jr-SM@users.noreply.github.com>
2024-02-15 08:58:20 -0400

    update script

--- static/script/script.js
+++ static/script/script.js
@@ -1,4 +1,7 @@
-document.getElementById('reactionButton').addEventListener('click', function() {
+var array_reaction = document.getElementsByClassName('reaction-button');
+
+for (var i = 0; i < array_reaction.length; i++) {
+  array_reaction[i].addEventListener('click', function () {
     var icon = this.querySelector('i');
     if (icon.classList.contains('bi-heart')) {
       icon.classList.remove('bi-heart');
@@ -8,4 +11,4 @@ document.getElementById('reactionButton').addEventListener('click', function() {
       icon.classList.add('bi-heart');
     }
   });
-  
+}
